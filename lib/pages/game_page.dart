95d7f80aa704f2d4d0697609131e68057ca7c217
Bruno Matheus Pereira Silva <82674117+bruno9800@users.noreply.github.com>
2023-07-06 12:35:05 -0300

    AppBar game whitout stars

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -14,16 +14,13 @@ class GamePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: CustomTheme.BackColor,
       appBar: AppBar(
+        toolbarHeight: 120,
+        centerTitle: true,
         backgroundColor: Colors.transparent,
-        toolbarHeight: 90,
         elevation: 0,
-        leading: CustomIconButton(
-          icon: FontAwesomeIcons.pause,
-          onPressed: () {
-            print('click');
-          },
+        automaticallyImplyLeading: false,
+        title: buildTitle(title: 'Level 5'),
         ),
-      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
@@ -35,5 +32,50 @@ class GamePage extends StatelessWidget {
       ),
     );
   }
+
+}
+Widget buildTitle({required String title}) {
+  return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CustomIconButton(
+          icon: FontAwesomeIcons.pause,
+          onPressed: () {
+            print('click');
+          },
+        ),
+        SizedBox(width: 48,),
+        Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 22, right: 22, top: 4, bottom: 4),
+              decoration: BoxDecoration(
+                  color: CustomTheme.ForeColor,
+                  borderRadius: BorderRadius.circular(16)
+              ),
+              child: Text('$title', style: TextStyle(color: CustomTheme.Black),),
+            ),
+            const SizedBox(height: 12,),
+            const Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('1'),
+                Text('2'),
+                Text('3')
+              ],
+            )
+          ],
+        ),
+        const SizedBox(width: 48,),
+        CustomIconButton(
+          icon: FontAwesomeIcons.house,
+          onPressed: () {
+            print('click');
+          },
+        ),
+      ],
+    );
 
 }
